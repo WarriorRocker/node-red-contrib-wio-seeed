e94128643920614b2696fec40373488089727214
robertbrown <robertbrown@zadv.com>
2016-06-14 21:00:20 -0400

    Fixed #1 adding reconnect on close and proper close on node close

--- wio-event/wio-event.js
+++ wio-event/wio-event.js
@@ -5,46 +5,64 @@
 		RED.nodes.createNode(this, config);
 		var node = this;
 		node.connection = RED.nodes.getCredentials(config.connection);
+		var socketConnection = null;
+		var attemptReconnect = true;
 
-		if (node.connection) {
-			var client = new WebSocketClient();
+		if (node.connection)
+			startConnection();
 
-			client.on('connect', function (connection) {
-				connection.send(config.node);
+		function startConnection() {
+			var socket = new WebSocketClient();
+			socket.on('connect', function (connection) {
+				socketConnection = connection;
+				handleConnection(connection);
+			});
+			socket.connect('wss://' + node.connection.server + '/v1/node/event');
+		}
 
-				connection.on('error', function (error) {
-					console.log(error);
-				});
+		node.on('close', function () {
+			attemptReconnect = false;
+			if (socketConnection)
+				socketConnection.close();
+		});
 
-				connection.on('close', function () {
-					console.log('closed');
-				});
+		function handleConnection(connection) {
+			connection.send(config.node);
 
-				connection.on('message', function (message) {
-					if ((message.type == 'utf8') && (message.utf8Data)) {
-						var json = JSON.parse(message.utf8Data);
-
-						if (config.output == 'value') {
-							var msgs = [];
-
-							for (var i = 0; i < config.events.length; i++)
-								msgs.push((((json.msg) && (json.msg[config.events[i]]))
-								? {
-									payload: json.msg[config.events[i]]
-								} : null));
-
-							if ((msgs.length > 1) || (msgs[0]))
-								node.send(msgs);
-						} else {
-							node.send({
-								payload: json
-							});
-						}
-					}
-				});
+			connection.on('error', function (error) {
+				node.status({ fill: 'red', shape: 'ring', text: error });
 			});
 
-			client.connect('wss://' + node.connection.server + '/v1/node/event');
+			connection.on('close', function () {
+				if (attemptReconnect)
+					setTimeout(function () {
+						startConnection();
+					}, 1000);
+			});
+
+			connection.on('message', function (message) {
+				node.status({});
+				if ((message.type == 'utf8') && (message.utf8Data)) {
+					var json = JSON.parse(message.utf8Data);
+
+					if (config.output == 'value') {
+						var msgs = [];
+
+						for (var i = 0; i < config.events.length; i++)
+							msgs.push((((json.msg) && (json.msg[config.events[i]]))
+							? {
+								payload: json.msg[config.events[i]]
+							} : null));
+
+						if ((msgs.length > 1) || (msgs[0]))
+							node.send(msgs);
+					} else {
+						node.send({
+							payload: json
+						});
+					}
+				}
+			});
 		}
 	}
 	RED.nodes.registerType('wio-event', WioEvent);
